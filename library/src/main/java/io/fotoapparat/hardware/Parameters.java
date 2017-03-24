14816ecb82e04d5eb98658040834de460a34f60a
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-03-24 23:39:11 +0100

    Added method stubs for Parameters.

--- library/src/main/java/io/fotoapparat/hardware/Parameters.java
+++ library/src/main/java/io/fotoapparat/hardware/Parameters.java
@@ -1,7 +1,30 @@
 package io.fotoapparat.hardware;
 
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
 /**
  * Parameters of {@link CameraDevice}.
  */
 public class Parameters {
+
+	public void putValue(Type type, Object value) {
+	}
+
+	public <T> T getValue(Type type) {
+		return null;
+	}
+
+	public List<Type> storedTypes() {
+		return emptyList();
+	}
+
+	public enum Type {
+
+		PICTURE_SIZE,
+		LENS_POSITION
+
+	}
+
 }
