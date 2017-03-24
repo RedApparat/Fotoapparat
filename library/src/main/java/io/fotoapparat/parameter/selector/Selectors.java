663e0633a355ebee7cca91d19cb84b8c1d1c146a
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-03-24 22:58:57 +0100

    Added Selectors.firstAvailable().

---
+++ library/src/main/java/io/fotoapparat/parameter/selector/Selectors.java
@@ -0,0 +1,17 @@
+package io.fotoapparat.parameter.selector;
+
+/**
+ * General selector functions.
+ */
+public class Selectors {
+
+	/**
+	 * Function which returns first non-null result from given selectors.
+	 *
+	 * @param functions functions in order of importance.
+	 */
+	public static <T> SelectorFunction<T> firstAvailable(SelectorFunction<T>... functions) {
+		return null;
+	}
+
+}
