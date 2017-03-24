85b0e919fe0d215f6b1b82e9815c2084051ce350
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-03-24 22:54:46 +0100

    Added CameraRenderer.

--- library/src/main/java/io/fotoapparat/FotoapparatBuilder.java
+++ library/src/main/java/io/fotoapparat/FotoapparatBuilder.java
@@ -3,11 +3,16 @@ package io.fotoapparat;
 import io.fotoapparat.parameter.LensPosition;
 import io.fotoapparat.parameter.Size;
 import io.fotoapparat.parameter.selector.SelectorFunction;
+import io.fotoapparat.view.CameraRenderer;
 
 /**
  * Builder for {@link Fotoapparat}.
  */
 public class FotoapparatBuilder {
+
+	public FotoapparatBuilder renderer(CameraRenderer renderer) {
+		return this;
+	}
 
     public FotoapparatBuilder photoSize(SelectorFunction<Size> selector) {
         return this;
---
+++ library/src/main/java/io/fotoapparat/view/CameraRenderer.java
@@ -0,0 +1,16 @@
+package io.fotoapparat.view;
+
+import io.fotoapparat.Fotoapparat;
+
+/**
+ * Renders the stream from {@link io.fotoapparat.Fotoapparat}.
+ */
+public interface CameraRenderer {
+
+	/**
+	 * Attaches renderer to camera, so that it will display the preview when camera is started. This
+	 * method will be called from camera thread, so it is safe to perform blocking operations here.
+	 */
+	void attachCamera(Fotoapparat camera);
+
+}
