b0c6f701c35d1e8a6eaf0df8f08aaba3bd934489
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-03-26 23:48:14 +0200

    Implemented FotoapparatBuilder. Added tests. Cleaned up build.gradle for library. Added Mockito.

--- library/src/main/java/io/fotoapparat/FotoapparatBuilder.java
+++ library/src/main/java/io/fotoapparat/FotoapparatBuilder.java
@@ -4,31 +4,49 @@ import io.fotoapparat.parameter.LensPosition;
 import io.fotoapparat.parameter.Size;
 import io.fotoapparat.parameter.selector.SelectorFunction;
 import io.fotoapparat.view.CameraRenderer;
-import io.fotoapparat.view.CameraView;
 
 /**
  * Builder for {@link Fotoapparat}.
  */
 public class FotoapparatBuilder {
 
-	public FotoapparatBuilder renderer(CameraRenderer renderer) {
-		return this;
+	CameraRenderer renderer;
+	SelectorFunction<Size> photoSizeSelector;
+	SelectorFunction<LensPosition> lensPositionSelector;
+
+	FotoapparatBuilder() {
+		// Do nothing
 	}
 
 	public FotoapparatBuilder photoSize(SelectorFunction<Size> selector) {
+		photoSizeSelector = selector;
 		return this;
 	}
 
 	public FotoapparatBuilder lensPosition(SelectorFunction<LensPosition> selector) {
+		lensPositionSelector = selector;
 		return this;
 	}
 
-	public FotoapparatBuilder into(CameraView texture) {
+	public FotoapparatBuilder into(CameraRenderer renderer) {
+		this.renderer = renderer;
 		return this;
 	}
 
 	public Fotoapparat build() {
+		validate();
+
 		return new Fotoapparat(this);
+	}
+
+	private void validate() {
+		if (renderer == null) {
+			throw new IllegalStateException("CameraRenderer is mandatory.");
+		}
+
+		if (lensPositionSelector == null) {
+			throw new IllegalStateException("LensPosition selector is mandatory.");
+		}
 	}
 
 }
