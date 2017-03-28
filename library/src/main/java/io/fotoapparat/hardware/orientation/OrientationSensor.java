23f4c89d5dc3fb4c49dffbecd5bee0152b7c70ef
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-03-28 21:29:03 +0200

    Implemented OrientationSensor.

---
+++ library/src/main/java/io/fotoapparat/hardware/orientation/OrientationSensor.java
@@ -0,0 +1,60 @@
+package io.fotoapparat.hardware.orientation;
+
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.support.annotation.NonNull;
+import android.view.OrientationEventListener;
+
+/**
+ * Monitors orientation of the device.
+ */
+public class OrientationSensor {
+
+	private final Listener listener;
+	private final OrientationEventListener orientationEventListener;
+
+	public OrientationSensor(@NonNull Context context,
+							 @NonNull final Listener listener) {
+		this.listener = listener;
+
+		orientationEventListener = new OrientationEventListener(context, SensorManager.SENSOR_DELAY_NORMAL) {
+
+			@Override
+			public void onOrientationChanged(int orientation) {
+				if (canDetectOrientation()) {
+					listener.onOrientationChanged(orientation);
+				}
+			}
+
+		};
+	}
+
+	/**
+	 * Starts monitoring device's orientation.
+	 */
+	public void start() {
+		orientationEventListener.enable();
+	}
+
+	/**
+	 * Stops monitoring device's orientation.
+	 */
+	public void stop() {
+		orientationEventListener.disable();
+	}
+
+	/**
+	 * Notified when orientation of the device is updated.
+	 */
+	public interface Listener {
+
+		/**
+		 * Called when orientation of the device is updated.
+		 *
+		 * @param degrees orientation of the device (relatively to user's eyes) in degrees.
+		 */
+		void onOrientationChanged(int degrees);
+
+	}
+
+}
