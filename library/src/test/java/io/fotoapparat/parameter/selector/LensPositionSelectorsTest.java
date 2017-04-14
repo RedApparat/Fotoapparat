2f0eace7006a05b9e60f360ed81097c4c86b4e21
Dionysis <ddl449@gmail.com>
2017-04-14 22:55:39 +0200

    Fix tests

--- library/src/test/java/io/fotoapparat/parameter/selector/LensPositionSelectorsTest.java
+++ library/src/test/java/io/fotoapparat/parameter/selector/LensPositionSelectorsTest.java
@@ -23,7 +23,7 @@ public class LensPositionSelectorsTest {
 
 		// When
 		LensPosition result = LensPositionSelectors
-				.front()
+				.lensPosition(LensPosition.FRONT)
 				.select(availablePositions);
 
 		// Then
@@ -43,7 +43,7 @@ public class LensPositionSelectorsTest {
 
 		// When
 		LensPosition result = LensPositionSelectors
-				.front()
+				.lensPosition(LensPosition.FRONT)
 				.select(availablePositions);
 
 		// Then
@@ -61,7 +61,7 @@ public class LensPositionSelectorsTest {
 
 		// When
 		LensPosition result = LensPositionSelectors
-				.back()
+				.lensPosition(LensPosition.BACK)
 				.select(availablePositions);
 
 		// Then
@@ -81,7 +81,7 @@ public class LensPositionSelectorsTest {
 
 		// When
 		LensPosition result = LensPositionSelectors
-				.back()
+				.lensPosition(LensPosition.BACK)
 				.select(availablePositions);
 
 		// Then
@@ -99,7 +99,7 @@ public class LensPositionSelectorsTest {
 
 		// When
 		LensPosition result = LensPositionSelectors
-				.external()
+				.lensPosition(LensPosition.EXTERNAL)
 				.select(availablePositions);
 
 		// Then
@@ -119,7 +119,7 @@ public class LensPositionSelectorsTest {
 
 		// When
 		LensPosition result = LensPositionSelectors
-				.external()
+				.lensPosition(LensPosition.EXTERNAL)
 				.select(availablePositions);
 
 		// Then
