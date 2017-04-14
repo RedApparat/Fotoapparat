71fdfd017332eb340ec1016defba3f35236dfa3a
Dionysis <ddl449@gmail.com>
2017-04-14 22:52:18 +0200

    Add more flexibility in the API

--- library/src/main/java/io/fotoapparat/parameter/selector/AspectRatioSelectors.java
+++ library/src/main/java/io/fotoapparat/parameter/selector/AspectRatioSelectors.java
@@ -1,7 +1,5 @@
 package io.fotoapparat.parameter.selector;
 
-import java.util.Collection;
-
 import io.fotoapparat.parameter.AspectRatio;
 
 /**
@@ -10,18 +8,11 @@ import io.fotoapparat.parameter.AspectRatio;
 public class AspectRatioSelectors {
 
 	/**
-	 * @return function which selects given {@link AspectRatio} from the list if it is available. If it is
-	 * not available provides {@code null}.
+	 * @return function which selects given {@link AspectRatio} from the list if it is available. If
+	 * it is not available provides {@code null}.
 	 */
 	public static SelectorFunction<AspectRatio> aspectRatio(final AspectRatio aspectRatio) {
-		return new SelectorFunction<AspectRatio>() {
-			@Override
-			public AspectRatio select(Collection<AspectRatio> items) {
-				return items.contains(aspectRatio)
-						? aspectRatio
-						: null;
-			}
-		};
+		return Selectors.single(aspectRatio);
 	}
 
 }
--- library/src/main/java/io/fotoapparat/parameter/selector/LensPositionSelectors.java
+++ library/src/main/java/io/fotoapparat/parameter/selector/LensPositionSelectors.java
@@ -1,7 +1,5 @@
 package io.fotoapparat.parameter.selector;
 
-import java.util.Collection;
-
 import io.fotoapparat.parameter.LensPosition;
 
 /**
@@ -10,38 +8,11 @@ import io.fotoapparat.parameter.LensPosition;
 public class LensPositionSelectors {
 
 	/**
-	 * @return {@link SelectorFunction} which provides the front camera if it is available.
-	 * Otherwise provides {@code null}.
+	 * @return function which selects given {@link LensPosition} from the list if it is available.
+	 * If it is not available - provides {@code null}.
 	 */
-	public static SelectorFunction<LensPosition> front() {
-		return lensPosition(LensPosition.FRONT);
-	}
-
-	/**
-	 * @return {@link SelectorFunction} which provides the back camera if it is available.
-	 * Otherwise provides {@code null}.
-	 */
-	public static SelectorFunction<LensPosition> back() {
-		return lensPosition(LensPosition.BACK);
-	}
-
-	/**
-	 * @return {@link SelectorFunction} which provides the external camera if it is available.
-	 * Otherwise provides {@code null}.
-	 */
-	public static SelectorFunction<LensPosition> external() {
-		return lensPosition(LensPosition.EXTERNAL);
-	}
-
-	private static SelectorFunction<LensPosition> lensPosition(final LensPosition position) {
-		return new SelectorFunction<LensPosition>() {
-			@Override
-			public LensPosition select(Collection<LensPosition> availablePositions) {
-				return availablePositions.contains(position)
-						? position
-						: null;
-			}
-		};
+	public static SelectorFunction<LensPosition> lensPosition(final LensPosition position) {
+		return Selectors.single(position);
 	}
 
 }
