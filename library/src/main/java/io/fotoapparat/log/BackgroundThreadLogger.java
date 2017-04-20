c71e6a0d88c80e3e35b3b8a0af54fca6a955bc30
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-04-20 22:49:36 +0200

    Implemented BackgroundThreadLogger.

---
+++ library/src/main/java/io/fotoapparat/log/BackgroundThreadLogger.java
@@ -0,0 +1,29 @@
+package io.fotoapparat.log;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Wrapper around another {@link Logger} which moves execution to another thread.
+ */
+class BackgroundThreadLogger implements Logger {
+
+	private static final Executor LOGGER_EXECUTOR = Executors.newSingleThreadExecutor();
+
+	private final Logger logger;
+
+	public BackgroundThreadLogger(Logger logger) {
+		this.logger = logger;
+	}
+
+	@Override
+	public void log(final String message) {
+		LOGGER_EXECUTOR.execute(new Runnable() {
+			@Override
+			public void run() {
+				logger.log(message);
+			}
+		});
+	}
+
+}
