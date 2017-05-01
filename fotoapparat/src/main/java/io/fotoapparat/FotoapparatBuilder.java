808d26d515641df232d8dac38bf2a1ff99c8c008
Dionysis <ddl449@gmail.com>
2017-05-02 00:30:08 +0200

    Default flash more is off instead of non-selected

--- fotoapparat/src/main/java/io/fotoapparat/FotoapparatBuilder.java
+++ fotoapparat/src/main/java/io/fotoapparat/FotoapparatBuilder.java
@@ -11,8 +11,8 @@ import io.fotoapparat.parameter.Flash;
 import io.fotoapparat.parameter.FocusMode;
 import io.fotoapparat.parameter.LensPosition;
 import io.fotoapparat.parameter.Size;
+import io.fotoapparat.parameter.selector.FlashSelectors;
 import io.fotoapparat.parameter.selector.SelectorFunction;
-import io.fotoapparat.parameter.selector.Selectors;
 import io.fotoapparat.preview.FrameProcessor;
 import io.fotoapparat.view.CameraRenderer;
 import io.fotoapparat.view.CameraView;
@@ -32,135 +32,135 @@ import static io.fotoapparat.parameter.selector.SizeSelectors.biggestSize;
  */
 public class FotoapparatBuilder {
 
-	Context context;
-	CameraProvider cameraProvider;
-	CameraRenderer renderer;
+    Context context;
+    CameraProvider cameraProvider;
+    CameraRenderer renderer;
 
-	SelectorFunction<LensPosition> lensPositionSelector = firstAvailable(
-			back(),
-			front(),
-			external()
-	);
-	SelectorFunction<Size> photoSizeSelector = biggestSize();
-	SelectorFunction<Size> previewSizeSelector = biggestSize();
-	SelectorFunction<FocusMode> focusModeSelector = firstAvailable(
-			continuousFocus(),
-			autoFocus(),
-			fixed()
-	);
-	SelectorFunction<Flash> flashSelector = Selectors.nothing();
+    SelectorFunction<LensPosition> lensPositionSelector = firstAvailable(
+            back(),
+            front(),
+            external()
+    );
+    SelectorFunction<Size> photoSizeSelector = biggestSize();
+    SelectorFunction<Size> previewSizeSelector = biggestSize();
+    SelectorFunction<FocusMode> focusModeSelector = firstAvailable(
+            continuousFocus(),
+            autoFocus(),
+            fixed()
+    );
+    SelectorFunction<Flash> flashSelector = FlashSelectors.off();
 
-	FrameProcessor frameProcessor = null;
+    FrameProcessor frameProcessor = null;
 
-	Logger logger = Loggers.none();
+    Logger logger = Loggers.none();
 
-	FotoapparatBuilder(@NonNull Context context) {
-		this.context = context;
+    FotoapparatBuilder(@NonNull Context context) {
+        this.context = context;
 
-		cameraProvider = defaultProvider(context);
-	}
+        cameraProvider = defaultProvider(context);
+    }
 
-	/**
-	 * @param cameraProvider decides which {@link CameraDevice} to use.
-	 */
-	public FotoapparatBuilder cameraProvider(CameraProvider cameraProvider) {
-		this.cameraProvider = cameraProvider;
-		return this;
-	}
+    /**
+     * @param cameraProvider decides which {@link CameraDevice} to use.
+     */
+    public FotoapparatBuilder cameraProvider(CameraProvider cameraProvider) {
+        this.cameraProvider = cameraProvider;
+        return this;
+    }
 
-	/**
-	 * @param selector selects size of the photo (in pixels) from list of available sizes.
-	 */
-	public FotoapparatBuilder photoSize(SelectorFunction<Size> selector) {
-		photoSizeSelector = selector;
-		return this;
-	}
+    /**
+     * @param selector selects size of the photo (in pixels) from list of available sizes.
+     */
+    public FotoapparatBuilder photoSize(SelectorFunction<Size> selector) {
+        photoSizeSelector = selector;
+        return this;
+    }
 
-	/**
-	 * @param selector selects size of preview stream (in pixels) from list of available sizes.
-	 */
-	public FotoapparatBuilder previewSize(SelectorFunction<Size> selector) {
-		previewSizeSelector = selector;
-		return this;
-	}
+    /**
+     * @param selector selects size of preview stream (in pixels) from list of available sizes.
+     */
+    public FotoapparatBuilder previewSize(SelectorFunction<Size> selector) {
+        previewSizeSelector = selector;
+        return this;
+    }
 
-	/**
-	 * @param selector selects focus mode from list of available modes.
-	 */
-	public FotoapparatBuilder focusMode(SelectorFunction<FocusMode> selector) {
-		focusModeSelector = selector;
-		return this;
-	}
+    /**
+     * @param selector selects focus mode from list of available modes.
+     */
+    public FotoapparatBuilder focusMode(SelectorFunction<FocusMode> selector) {
+        focusModeSelector = selector;
+        return this;
+    }
 
-	/**
-	 * @param selector selects flash mode from list of available modes.
-	 */
-	public FotoapparatBuilder flash(SelectorFunction<Flash> selector) {
-		flashSelector = selector;
-		return this;
-	}
+    /**
+     * @param selector selects flash mode from list of available modes.
+     */
+    public FotoapparatBuilder flash(SelectorFunction<Flash> selector) {
+        flashSelector = selector;
+        return this;
+    }
 
-	/**
-	 * @param selector camera sensor position from list of available positions.
-	 */
-	public FotoapparatBuilder lensPosition(SelectorFunction<LensPosition> selector) {
-		lensPositionSelector = selector;
-		return this;
-	}
+    /**
+     * @param selector camera sensor position from list of available positions.
+     */
+    public FotoapparatBuilder lensPosition(SelectorFunction<LensPosition> selector) {
+        lensPositionSelector = selector;
+        return this;
+    }
 
-	/**
-	 * @param frameProcessor receives preview frames for processing.
-	 * @see FrameProcessor
-	 */
-	public FotoapparatBuilder frameProcessor(FrameProcessor frameProcessor) {
-		this.frameProcessor = frameProcessor;
-		return this;
-	}
+    /**
+     * @param frameProcessor receives preview frames for processing.
+     * @see FrameProcessor
+     */
+    public FotoapparatBuilder frameProcessor(FrameProcessor frameProcessor) {
+        this.frameProcessor = frameProcessor;
+        return this;
+    }
 
-	/**
-	 * @param logger logger which will print logs. No logger is set by default.
-	 * @see Loggers
-	 */
-	public FotoapparatBuilder logger(Logger logger) {
-		this.logger = logger;
-		return this;
-	}
+    /**
+     * @param logger logger which will print logs. No logger is set by default.
+     * @see Loggers
+     */
+    public FotoapparatBuilder logger(Logger logger) {
+        this.logger = logger;
+        return this;
+    }
 
-	/**
-	 * @param renderer view which will draw the stream from the camera.
-	 * @see CameraView
-	 */
-	public FotoapparatBuilder into(CameraRenderer renderer) {
-		this.renderer = renderer;
-		return this;
-	}
+    /**
+     * @param renderer view which will draw the stream from the camera.
+     * @see CameraView
+     */
+    public FotoapparatBuilder into(CameraRenderer renderer) {
+        this.renderer = renderer;
+        return this;
+    }
 
-	/**
-	 * @return set up instance of {@link Fotoapparat}.
-	 * @throws IllegalStateException if some mandatory parameters are not specified.
-	 */
-	public Fotoapparat build() {
-		validate();
+    /**
+     * @return set up instance of {@link Fotoapparat}.
+     * @throws IllegalStateException if some mandatory parameters are not specified.
+     */
+    public Fotoapparat build() {
+        validate();
 
-		return Fotoapparat.create(this);
-	}
+        return Fotoapparat.create(this);
+    }
 
-	private void validate() {
-		if (cameraProvider == null) {
-			throw new IllegalStateException("CameraProvider is mandatory.");
-		}
+    private void validate() {
+        if (cameraProvider == null) {
+            throw new IllegalStateException("CameraProvider is mandatory.");
+        }
 
-		if (renderer == null) {
-			throw new IllegalStateException("CameraRenderer is mandatory.");
-		}
+        if (renderer == null) {
+            throw new IllegalStateException("CameraRenderer is mandatory.");
+        }
 
-		if (lensPositionSelector == null) {
-			throw new IllegalStateException("LensPosition selector is mandatory.");
-		}
+        if (lensPositionSelector == null) {
+            throw new IllegalStateException("LensPosition selector is mandatory.");
+        }
 
-		if (photoSizeSelector == null) {
-			throw new IllegalStateException("Photo size selector is mandatory.");
-		}
-	}
+        if (photoSizeSelector == null) {
+            throw new IllegalStateException("Photo size selector is mandatory.");
+        }
+    }
 
 }
