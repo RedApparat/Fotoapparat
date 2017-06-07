f69c9e9352198324d124ffcd56f6bc6e8784ea9d
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-06-07 23:23:28 +0200

    Always including FocusMode.FIXED on Camera1

--- fotoapparat/src/main/java/io/fotoapparat/hardware/v1/capabilities/CapabilitiesFactory.java
+++ fotoapparat/src/main/java/io/fotoapparat/hardware/v1/capabilities/CapabilitiesFactory.java
@@ -21,69 +21,71 @@ import io.fotoapparat.parameter.Size;
 @SuppressWarnings("deprecation")
 public class CapabilitiesFactory {
 
-	/**
-	 * @return {@link Capabilities} from given camera parameters.
-	 */
-	public Capabilities fromParameters(Camera.Parameters parameters) {
-		return new Capabilities(
-				extractPictureSizes(parameters),
-				extractPreviewSizes(parameters),
-				extractFocusModes(parameters),
-				extractFlashModes(parameters)
-		);
-	}
+    /**
+     * @return {@link Capabilities} from given camera parameters.
+     */
+    public Capabilities fromParameters(Camera.Parameters parameters) {
+        return new Capabilities(
+                extractPictureSizes(parameters),
+                extractPreviewSizes(parameters),
+                extractFocusModes(parameters),
+                extractFlashModes(parameters)
+        );
+    }
 
-	private Set<Size> extractPreviewSizes(Camera.Parameters parameters) {
-		return mapSizes(parameters.getSupportedPreviewSizes());
-	}
+    private Set<Size> extractPreviewSizes(Camera.Parameters parameters) {
+        return mapSizes(parameters.getSupportedPreviewSizes());
+    }
 
-	private Set<Size> extractPictureSizes(Camera.Parameters parameters) {
-		return mapSizes(parameters.getSupportedPictureSizes());
-	}
+    private Set<Size> extractPictureSizes(Camera.Parameters parameters) {
+        return mapSizes(parameters.getSupportedPictureSizes());
+    }
 
-	private Set<Size> mapSizes(Collection<Camera.Size> sizes) {
-		HashSet<Size> result = new HashSet<>();
+    private Set<Size> mapSizes(Collection<Camera.Size> sizes) {
+        HashSet<Size> result = new HashSet<>();
 
-		for (Camera.Size size : sizes) {
-			result.add(new Size(
-					size.width,
-					size.height
-			));
-		}
+        for (Camera.Size size : sizes) {
+            result.add(new Size(
+                    size.width,
+                    size.height
+            ));
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private Set<Flash> extractFlashModes(Camera.Parameters parameters) {
-		HashSet<Flash> result = new HashSet<>();
+    private Set<Flash> extractFlashModes(Camera.Parameters parameters) {
+        HashSet<Flash> result = new HashSet<>();
 
-		for (String flashMode : supportedFlashModes(parameters)) {
-			result.add(
-					FlashCapability.toFlash(flashMode)
-			);
-		}
+        for (String flashMode : supportedFlashModes(parameters)) {
+            result.add(
+                    FlashCapability.toFlash(flashMode)
+            );
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	@NonNull
-	private List<String> supportedFlashModes(Camera.Parameters parameters) {
-		List<String> supportedFlashModes = parameters.getSupportedFlashModes();
-		return supportedFlashModes != null
-				? supportedFlashModes
-				: Collections.singletonList(Camera.Parameters.FLASH_MODE_OFF);
-	}
+    @NonNull
+    private List<String> supportedFlashModes(Camera.Parameters parameters) {
+        List<String> supportedFlashModes = parameters.getSupportedFlashModes();
+        return supportedFlashModes != null
+                ? supportedFlashModes
+                : Collections.singletonList(Camera.Parameters.FLASH_MODE_OFF);
+    }
 
-	private Set<FocusMode> extractFocusModes(Camera.Parameters parameters) {
-		HashSet<FocusMode> result = new HashSet<>();
+    private Set<FocusMode> extractFocusModes(Camera.Parameters parameters) {
+        HashSet<FocusMode> result = new HashSet<>();
 
-		for (String focusMode : parameters.getSupportedFocusModes()) {
-			result.add(
-					FocusCapability.toFocusMode(focusMode)
-			);
-		}
+        for (String focusMode : parameters.getSupportedFocusModes()) {
+            result.add(
+                    FocusCapability.toFocusMode(focusMode)
+            );
+        }
 
-		return result;
-	}
+        result.add(FocusMode.FIXED);
+
+        return result;
+    }
 
 }
