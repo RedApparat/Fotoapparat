e89a06e4e07ebd47b361c4403870a75af4f0da6a
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-07-14 22:35:27 +0200

    [issue-56] Ignoring CameraException when trying to restart preview after taking a photo.

--- fotoapparat/src/main/java/io/fotoapparat/routine/picture/TakePictureTask.java
+++ fotoapparat/src/main/java/io/fotoapparat/routine/picture/TakePictureTask.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 import io.fotoapparat.hardware.CameraDevice;
+import io.fotoapparat.hardware.CameraException;
 import io.fotoapparat.lens.FocusResult;
 import io.fotoapparat.photo.Photo;
 
@@ -12,30 +13,49 @@ import io.fotoapparat.photo.Photo;
  */
 class TakePictureTask extends FutureTask<Photo> {
 
+    private static final int MAX_FOCUS_ATTEMPTS = 3;
+
     TakePictureTask(final CameraDevice cameraDevice) {
         super(new Callable<Photo>() {
             @Override
             public Photo call() throws Exception {
-
-                int focusAttempts = 0;
-                FocusResult focusResult = FocusResult.none();
-
-                while (focusAttempts < 3 && !focusResult.succeeded) {
-                    focusResult = cameraDevice.autoFocus();
-                    focusAttempts++;
-                }
-
-                if (focusResult.needsExposureMeasurement) {
-                    cameraDevice.measureExposure();
-                }
+                adjustCameraForBestShot(cameraDevice);
 
                 Photo photo = cameraDevice.takePicture();
 
-                cameraDevice.startPreview();
+                startPreviewSafe(cameraDevice);
 
                 return photo;
             }
         });
+    }
+
+    private static void adjustCameraForBestShot(CameraDevice cameraDevice) {
+        FocusResult focusResult = autoFocus(cameraDevice);
+
+        if (focusResult.needsExposureMeasurement) {
+            cameraDevice.measureExposure();
+        }
+    }
+
+    private static FocusResult autoFocus(CameraDevice cameraDevice) {
+        int focusAttempts = 0;
+        FocusResult focusResult = FocusResult.none();
+
+        while (focusAttempts < MAX_FOCUS_ATTEMPTS && !focusResult.succeeded) {
+            focusResult = cameraDevice.autoFocus();
+            focusAttempts++;
+        }
+
+        return focusResult;
+    }
+
+    private static void startPreviewSafe(CameraDevice cameraDevice) {
+        try {
+            cameraDevice.startPreview();
+        } catch (CameraException e) {
+            // Do nothing
+        }
     }
 
 }
--- fotoapparat/src/test/java/io/fotoapparat/routine/picture/TakePictureTaskTest.java
+++ fotoapparat/src/test/java/io/fotoapparat/routine/picture/TakePictureTaskTest.java
@@ -9,15 +9,17 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import io.fotoapparat.hardware.CameraDevice;
+import io.fotoapparat.hardware.CameraException;
 import io.fotoapparat.lens.FocusResult;
 import io.fotoapparat.photo.Photo;
-import io.fotoapparat.routine.picture.TakePictureTask;
 
 import static io.fotoapparat.test.TestUtils.resultOf;
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TakePictureTaskTest {
@@ -90,4 +92,24 @@ public class TakePictureTaskTest {
 
         assertEquals(result, PHOTO);
     }
+
+    @Test
+    public void startPreviewFailed() throws Exception {
+        // Given
+        given(cameraDevice.autoFocus())
+                .willReturn(new FocusResult(true, false));
+
+        doThrow(new CameraException("test"))
+                .when(cameraDevice)
+                .startPreview();
+
+        // When
+        Photo result = resultOf(testee);
+
+        // Then
+        verify(cameraDevice).startPreview();
+
+        assertEquals(result, PHOTO);
+    }
+
 }
