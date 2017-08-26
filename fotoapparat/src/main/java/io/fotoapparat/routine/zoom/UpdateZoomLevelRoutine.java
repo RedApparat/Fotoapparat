180c238ae2f5b691296ee8595f69f29afe3e00c4
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2017-08-26 16:10:17 +0200

    Implemented UpdateZoomLevelRoutine.

--- fotoapparat/src/main/java/io/fotoapparat/routine/zoom/UpdateZoomLevelRoutine.java
+++ fotoapparat/src/main/java/io/fotoapparat/routine/zoom/UpdateZoomLevelRoutine.java
@@ -21,6 +21,27 @@ public class UpdateZoomLevelRoutine {
      * @param zoomLevel zoom level of the camera. Value between 0 and 1.
      */
     public void updateZoomLevel(@FloatRange(from = 0f, to = 1f) float zoomLevel) {
+        ensureInBounds(zoomLevel);
+
+        if (cameraDevice.getCapabilities().isZoomSupported()) {
+            cameraDevice.setZoom(zoomLevel);
+        }
+    }
+
+    private void ensureInBounds(float zoomLevel) {
+        if (zoomLevel < 0f || zoomLevel > 1f) {
+            throw new LevelOutOfRangeException(zoomLevel);
+        }
+    }
+
+    /**
+     * Thrown when zoom level is outside of [0..1] range.
+     */
+    static class LevelOutOfRangeException extends RuntimeException {
+
+        public LevelOutOfRangeException(float zoomLevel) {
+            super(zoomLevel + " is out of range [0..1]");
+        }
 
     }
 
