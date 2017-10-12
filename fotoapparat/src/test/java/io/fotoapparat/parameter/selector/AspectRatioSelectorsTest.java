e1b4d46d92653bc24bfd5c35368f4501c9f3c5e2
Krzysztof Narkiewicz <krzysztof.narkiewicz@ezaquarii.com>
2017-10-12 05:34:02 +0100

    Aspect ratio selector with tolerance
    Some resolutions supported by camera CCD have aspect ratios
    that deviate from 16:9 and 4:3 by a small margin (ex. 16:10).
    
    Add tolerance to aspect ratio selector, to catch all picture
    sizses that would be otherwise rejected due to strict
    comparison.

--- fotoapparat/src/test/java/io/fotoapparat/parameter/selector/AspectRatioSelectorsTest.java
+++ fotoapparat/src/test/java/io/fotoapparat/parameter/selector/AspectRatioSelectorsTest.java
@@ -50,6 +50,38 @@ public class AspectRatioSelectorsTest {
     }
 
     @Test
+    public void standardRatioWithTolerance() throws Exception {
+        // Given
+        given(sizeSelector.select(ArgumentMatchers.<Size>anyCollection()))
+                .willReturn(new Size(400, 300));
+
+        // When
+        Size result = AspectRatioSelectors
+                .standardRatio(sizeSelector, 0.1)
+                .select(asList(
+                        new Size(400, 300),
+                        new Size(410, 300),
+                        new Size(800, 600),
+                        new Size(790, 600),
+                        new Size(100, 100),
+                        new Size(160, 90)
+                ));
+
+        // Then
+        assertEquals(
+                new Size(400, 300),
+                result
+        );
+
+        verify(sizeSelector).select(asSet(
+                new Size(400, 300),
+                new Size(410, 300),
+                new Size(800, 600),
+                new Size(790, 600)
+        ));
+    }
+
+    @Test
     public void wideRatio() throws Exception {
         // Given
         given(sizeSelector.select(ArgumentMatchers.<Size>anyCollection()))
@@ -73,6 +105,70 @@ public class AspectRatioSelectorsTest {
         verify(sizeSelector).select(asSet(
                 new Size(16, 9),
                 new Size(32, 18)
+        ));
+    }
+
+    @Test
+    public void wideRatioWithTolerance() throws Exception {
+        // Given
+        given(sizeSelector.select(ArgumentMatchers.<Size>anyCollection()))
+                .willReturn(new Size(16, 9));
+
+        // When
+        Size result = AspectRatioSelectors
+                .wideRatio(sizeSelector, 0.1)
+                .select(asList(
+                        new Size(16, 9),
+                        new Size(16, 10),
+                        new Size(32, 18),
+                        new Size(32, 20),
+                        new Size(10, 10)
+                ));
+
+        // Then
+        assertEquals(
+                new Size(16, 9),
+                result
+        );
+
+        verify(sizeSelector).select(asSet(
+                new Size(16, 9),
+                new Size(16, 10),
+                new Size(32, 18),
+                new Size(32, 20)
+        ));
+    }
+
+    @Test
+    public void ratioWithTolerance() throws Exception {
+        // Given
+        given(sizeSelector.select(ArgumentMatchers.<Size>anyCollection()))
+                .willReturn(new Size(110, 100));
+
+        // When
+        Size result = AspectRatioSelectors
+                .aspectRatio(1.0f, sizeSelector, 0.1)
+                .select(asList(
+                        new Size(16, 9),
+                        new Size(110, 100),
+                        new Size(105, 100),
+                        new Size(95, 100),
+                        new Size(90, 100),
+                        new Size(4, 3),
+                        new Size(20, 10)
+                ));
+
+        // Then
+        assertEquals(
+                new Size(110, 100),
+                result
+        );
+
+        verify(sizeSelector).select(asSet(
+                new Size(110, 100),
+                new Size(105, 100),
+                new Size(95, 100),
+                new Size(90, 100)
         ));
     }
 
