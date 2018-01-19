58ffc4f9ea1c2c14d118ed16c6c0be992b2ef1fb
Dionysis <ddl449@gmail.com>
2018-01-19 16:37:42 +0100

    Add forgotten var assignments. Fixes #183

--- fotoapparat/src/main/java/io/fotoapparat/configuration/UpdateConfiguration.kt
+++ fotoapparat/src/main/java/io/fotoapparat/configuration/UpdateConfiguration.kt
@@ -26,55 +26,55 @@ data class UpdateConfiguration(
         private var configuration = UpdateConfiguration()
 
         fun flash(selector: FlashSelector): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     flashMode = selector
             )
         }
 
         fun focusMode(selector: FocusModeSelector): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     focusMode = selector
             )
         }
 
         fun previewFpsRange(selector: FpsRangeSelector): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     previewFpsRange = selector
             )
         }
 
         fun sensorSensitivity(selector: SensorSensitivitySelector): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     sensorSensitivity = selector
             )
         }
 
         fun antiBandingMode(selector: AntiBandingModeSelector): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     antiBandingMode = selector
             )
         }
 
         fun jpegQuality(selector: QualitySelector): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     jpegQuality = selector
             )
         }
 
         fun previewResolution(selector: ResolutionSelector): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     previewResolution = selector
             )
         }
 
         fun photoResolution(selector: ResolutionSelector): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     pictureResolution = selector
             )
         }
 
         fun frameProcessor(frameProcessor: FrameProcessor): Builder = apply {
-            configuration.copy(
+            configuration = configuration.copy(
                     frameProcessor = frameProcessor
             )
         }
