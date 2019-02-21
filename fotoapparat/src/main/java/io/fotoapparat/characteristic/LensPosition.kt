9fd03a83a24748b9e1fc6c9e680633e9e750015a
Nicklas Ansman Giertz <nicklas@ansman.se>
2019-02-21 10:14:02 -0500

    Make all the configuration objects have nicer toString values

--- fotoapparat/src/main/java/io/fotoapparat/characteristic/LensPosition.kt
+++ fotoapparat/src/main/java/io/fotoapparat/characteristic/LensPosition.kt
@@ -8,16 +8,22 @@ sealed class LensPosition : Characteristic {
     /**
      * The back camera.
      */
-    object Back : LensPosition()
+    object Back : LensPosition() {
+        override fun toString(): String = "LensPosition.Back"
+    }
 
     /**
      * The front camera.
      */
-    object Front : LensPosition()
+    object Front : LensPosition() {
+        override fun toString(): String = "LensPosition.Front"
+    }
 
     /**
      * An external camera.
      */
-    object External : LensPosition()
+    object External : LensPosition() {
+        override fun toString(): String = "LensPosition.External"
+    }
 
 }
