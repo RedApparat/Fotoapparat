b1f6f491cdc5cb202468d0e97ccecbdb79e05159
Nicklas Ansman Giertz <nicklas@ansman.se>
2019-03-02 06:32:19 -0500

    Make all the configuration objects have nicer toString values (#348)

--- fotoapparat/src/main/java/io/fotoapparat/hardware/orientation/Orientation.kt
+++ fotoapparat/src/main/java/io/fotoapparat/hardware/orientation/Orientation.kt
@@ -18,12 +18,16 @@ sealed class Orientation(
         /**
          * A vertical, normal orientation.
          */
-        object Portrait : Vertical(0)
+        object Portrait : Vertical(0) {
+            override fun toString(): String = "Orientation.Vertical.Portrait"
+        }
 
         /**
          * A reversed (flipped phone) orientation.
          */
-        object ReversePortrait : Vertical(180)
+        object ReversePortrait : Vertical(180) {
+            override fun toString(): String = "Orientation.Vertical.ReversePortrait"
+        }
 
     }
 
@@ -35,12 +39,16 @@ sealed class Orientation(
         /**
          * A 90 degrees clockwise from "normal", orientation.
          */
-        object Landscape : Horizontal(90)
+        object Landscape : Horizontal(90) {
+            override fun toString(): String = "Orientation.Horizontal.Landscape"
+        }
 
         /**
          * A 90 degrees counter-clockwise from "normal", orientation.
          */
-        object ReverseLandscape : Horizontal(270)
+        object ReverseLandscape : Horizontal(270) {
+            override fun toString(): String = "Orientation.Horizontal.ReverseLandscape"
+        }
 
     }
 }
