9cadd65b9022f698d4afb6c8bfc287b0b77356b9
Denver Braganza <braganza.denver@gmail.com>
2023-03-14 20:14:28 +0530

    Updated libraries but still not working

--- fotoapparat/src/main/java/io/fotoapparat/hardware/display/Display.kt
+++ fotoapparat/src/main/java/io/fotoapparat/hardware/display/Display.kt
@@ -14,12 +14,12 @@ import io.fotoapparat.hardware.orientation.Orientation.Vertical.ReversePortrait
  */
 internal open class Display(context: Context) {
 
-    private val display = context.getDisplay()
+    private val display = context.getDisplay1()
 
     /**
      * Returns the orientation of the screen.
      */
-    open fun getOrientation(): Orientation = when (display.rotation) {
+    open fun getOrientation(): Orientation = when (display?.rotation) {
         Surface.ROTATION_0 -> Portrait
         Surface.ROTATION_90 -> Landscape
         Surface.ROTATION_180 -> ReversePortrait
@@ -29,4 +29,4 @@ internal open class Display(context: Context) {
 
 }
 
-private fun Context.getDisplay() = (getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay
+private fun Context.getDisplay1() = (getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay
